c6eec1954ce37d8f668b079123a5196802b8024f
Sarvesh Kumbharde <sarveshkumbharde@gmail.com>
2026-04-18 20:20:45 +0530

    xx

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cors({
   if (!origin || allowedOrigins.includes(origin)) {
     callback(null, true);
   } else {
-    callback(new Error("Not allowed by CORS"));
+    callback(new Error("Not allowed by CORS"));       
   }           
 }
 }))
