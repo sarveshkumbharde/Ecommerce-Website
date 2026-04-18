5d94e01ce304fad5362f82702c4bbc129dd9bdeb
Sarvesh Kumbharde <sarveshkumbharde@gmail.com>
2026-04-18 22:15:22 +0530

    xcv

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connectCloudinary()
 const allowedOrigins = process.env.FRONTEND_URLS.split(", "); 
 app.use(cors({
     origin: function(origin, callback) {
-  if (!origin || allowedOrigins.includes(origin)) {
+  if (!origin || allowedOrigins.includes(origin)) {        
     callback(null, true);
   } else {
     callback(new Error("Not allowed by CORS"));       
