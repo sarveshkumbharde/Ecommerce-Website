609015926cb613d5e60e8ac73f863c8e8dd14622
Sarvesh Kumbharde <sarveshkumbharde@gmail.com>
2026-04-18 20:41:49 +0530

    xx

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ import orderRouter from './routes/order.routes.js'
 //App Config
 const app = express()
 const port = process.env.PORT || 4000
-connectDb()
+connectDb()  
 connectCloudinary()
 
 //middlewares
